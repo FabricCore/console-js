ba8c16593ff2eb3b38494a5db906badb34c3fb67
siriusmart <siriusmart@tuta.io>
2025-06-23 23:14:07 +0100

    cleaned prelude

--- prelude.js
+++ prelude.js
@@ -1,5 +1,1 @@
-let net = Packages.ws.siri.jscore.mapping.JSPackage.getRoot().net;
-let MinecraftClient = net.minecraft.client.MinecraftClient;
-let Text = net.minecraft.text.Text;
-
 prelude.eval("let console = module.require('/modules/console')", "one");
